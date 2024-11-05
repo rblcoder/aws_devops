cf20edab1db2b61e2eac452e075c885536e530a3
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2024-11-05 12:03:32 +0000

    rds terraform

---
+++ rds-terraform/main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow PostgreSQL inbound traffic"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Data source to fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data source to fetch all subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Subnet Group
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = data.aws_subnets.default.ids
+
+  tags = {
+    Name = "MyDBSubnetGroup"
+  }
+}
+
+# RDS Instance
+resource "aws_db_instance" "db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "13.3"
+  instance_class       = "db.t2.micro"
+  name                 = "ecommerceprod"
+  db_name              = "ecommerceprod"
+  username             = "postgres"
+  password             = "password"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "MyPublicPostgresDB"
+  }
+}
+
+# Output instance endpoint
+output "db_instance_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.db.endpoint
+}
